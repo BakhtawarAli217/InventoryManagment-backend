35d294b43a956b7b3c6f6b7d85fbf6963459a97d
pakelegence <pakelegence@gmail.com>
2026-07-25 21:06:28 +0500

    correct the cookie save section

--- app.js
+++ app.js
@@ -25,7 +25,10 @@ app.use(session({
     resave:false,
     saveUninitialized:false,
     cookie:{
-        maxAge:30 * 24 * 60 * 60 * 1000
+        maxAge:30 * 24 * 60 * 60 * 1000,
+        httpOnly:true,
+        sameSite:"none",
+        secure:true
     }
 }))
 app.use(passport.initialize())
