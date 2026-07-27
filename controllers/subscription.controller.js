080ef97ad54ce5dd8ee92c37cd604cbad96f4b0f
pakelegence <pakelegence@gmail.com>
2026-07-27 12:54:20 +0500

    resolve the error

--- controllers/subscription.controller.js
+++ controllers/subscription.controller.js
@@ -1,29 +1,34 @@
 const prisma=require("../prismaClient")
 
+module.exports.findSubscriptionByUserId = async (req, res) => {
+  try {
+    const { id } = req.query;
 
-module.exports.findSubscriptionByUserId=async (req,res)=>{
-    try{
-        const {id}=req.query
-        if(!id){
-            return res.status(400).json({message:"User id is required"})
-        }
-       
-        const subscription=await prisma.order.findFirst({
-            where:{
-                userId:id
-            }
-        })
-        if(!subscription){
-        return res.status(404).json({message:"User have no subscription"})
-        }
-        if(subscription.status==="paid"){
-            return res.status(200).json({message:"active"})
-        }
-        if(subscription.status==="pending"){
-            return res.status(200).json({message:"Pending payment status"})
-        }
-        
-    }catch(e){
-        return res.status(500).json({message:"Internal Server Error" , error:e.message})
+    const subscription = await prisma.order.findFirst({
+      where: {
+        userId: id,
+      },
+    });
+
+    
+
+    if (!subscription) {
+      return res.status(404).json({
+        message: "User has no subscription",
+      });
     }
-}
+
+
+    return res.status(200).json({
+      message: "Sucsription fetched successfully" , data:subscription
+    });
+
+  } catch (e) {
+    console.error(e);
+
+    return res.status(500).json({
+      message: "Internal Server Error",
+      error: e.message,
+    });
+  }
+};
